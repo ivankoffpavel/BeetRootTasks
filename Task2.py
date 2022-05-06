1fe9ba3ad15b61fc06da45eeb026806bde1661ed
ivankoffpavel <ivankoffpavel@gmail.com>
2022-05-06 18:43:47 +0300

    Task2,Task3 send to GitHub

---
+++ Task2.py
@@ -0,0 +1,23 @@
+#1_2_3_4_5
+print(1,2,3,4,5,sep='_')
+#Hello friend! Nice to meet you
+
+
+#I'am fine
+print('Hello friend',end='! ')
+print('Nice to meet you\n\n\n')
+print("I'am fine")
+#2.33 2 foo 6 fine day None False
+print(2.33,2,'foo',2+4,'fine day',None,False)
+#***			***
+print('***\t\t\t***')
+#Help on built - in function print in module builtins:
+#print(...)
+#print(value, ..., sep=' ', end='\n', file=sys.stdout, flush=False)
+#Prints the values to a stream, or to sys.stdout by default.
+#Optional keyword arguments:
+#file: a file - like object(stream);defaults to the current sys.stdout.
+#sep: string inserted between values, default a space.
+#end: string appended after the last value, default a newline.
+#flush: whether to forcibly flush the stream.
+help(print)
